49a6454817f62ba7767ff8bacc5692467fe9290d
Chiel Douwes <chieldouwes@gmail.com>
2020-02-14 01:44:02 +0100

    Add test for indexing with policy

--- source/test/week1/Image.hpp
+++ source/test/week1/Image.hpp
@@ -9,6 +9,7 @@
 bool test_image() {
 	// construct the image object
 	Image<char, 10, 10> image{};
-	image.get(1, 2) = 1;
+	image.get(1, 2)               = 1;
+	image.get(NullPolicy{}, 2, 2) = 1;
 	return true;
 }
