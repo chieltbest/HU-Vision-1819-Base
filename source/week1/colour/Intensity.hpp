d9338c5d7517335659bc85fd0c418e7588f5ba61
Chiel Douwes <chieldouwes@gmail.com>
2020-02-14 03:23:04 +0100

    Fix conversion operators and add tests for them

--- source/week1/colour/Intensity.hpp
+++ source/week1/colour/Intensity.hpp
@@ -4,12 +4,22 @@
 
 #pragma once // VISION_GRAYSCALE_HPP
 
+#include "RGB.hpp"
+
 template <typename ValueType>
 class Intensity {
 public:
 	ValueType value;
 
+	Intensity() : value{} {
+	}
+
 	template <typename VType>
 	Intensity(VType &&value) : value{value} {
+	}
+
+	Intensity(RGB<ValueType> rgb)
+	    : // linear RGB to Luminance
+	      value{ValueType(.2126 * rgb.r + .7152 * rgb.g + .0722 * rgb.b)} {
 	}
 };
