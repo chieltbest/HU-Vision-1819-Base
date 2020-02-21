6fb4d224cb7170b6570c9a1300bf91543d7e8467
Chiel Douwes <chieldouwes@gmail.com>
2020-02-21 10:58:26 +0100

    Make RGBImageStudent use throwing out of bounds policy

--- source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
+++ source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
@@ -1,36 +1,35 @@
 #include "RGBImageStudent.h"
 
-RGBImageStudent::RGBImageStudent() : DynamicImage<RGB, RGBImage>{} {
+RGBImageStudent::RGBImageStudent() : Base{} {
 }
 
 RGBImageStudent::RGBImageStudent(const RGBImageStudent &other) = default;
 
-RGBImageStudent::RGBImageStudent(const int width, const int height)
-    : DynamicImage<RGB, RGBImage>{width, height} {
+RGBImageStudent::RGBImageStudent(const int width, const int height) : Base{width, height} {
 }
 
 RGBImageStudent::~RGBImageStudent() = default;
 
 void RGBImageStudent::set(const int width, const int height) {
-	DynamicImage<RGB, RGBImage>::set(width, height);
+	Base::set(width, height);
 }
 
 void RGBImageStudent::set(const RGBImageStudent &other) {
-	DynamicImage<RGB, RGBImage>::set(other);
+	Base::set(other);
 }
 
 void RGBImageStudent::setPixel(int x, int y, RGB pixel) {
-	DynamicImage<RGB, RGBImage>::setPixel(x, y, pixel);
+	Base::setPixel(x, y, pixel);
 }
 
 void RGBImageStudent::setPixel(int i, RGB pixel) {
-	DynamicImage<RGB, RGBImage>::setPixel(i, pixel);
+	Base::setPixel(i, pixel);
 }
 
 RGB RGBImageStudent::getPixel(int x, int y) const {
-	return DynamicImage<RGB, RGBImage>::getPixel(x, y);
+	return Base::getPixel(x, y);
 }
 
 RGB RGBImageStudent::getPixel(int i) const {
-	return DynamicImage<RGB, RGBImage>::getPixel(i);
+	return Base::getPixel(i);
 }
